3c0312e4f12e02a679f61274b8e160b1b93c1e93
Dallas Wyciskalla <dallas.wyciskalla@gmail.com>
2024-06-29 11:04:22 -0500

    added api from start spring io

---
+++ dwyci-fitness-api/dwyci-fitness/src/main/java/com/dwyci_fitness/dwyci_fitness/DwyciFitnessApplication.java
@@ -0,0 +1,13 @@
+package com.dwyci_fitness.dwyci_fitness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DwyciFitnessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DwyciFitnessApplication.class, args);
+	}
+
+}
---
+++ dwyci-fitness-api/dwyci-fitness/src/test/java/com/dwyci_fitness/dwyci_fitness/DwyciFitnessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dwyci_fitness.dwyci_fitness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DwyciFitnessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
